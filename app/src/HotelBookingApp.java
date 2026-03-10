571af519bdc1e63df640be17f31c97a46c2b7ccc
2006veda <movvaveda8@gmail.com>
2026-03-10 09:39:33 +0530

    Usecase2

--- app/src/HotelBookingApp.java
+++ app/src/HotelBookingApp.java
@@ -1,8 +1,82 @@
+
+
+abstract class Room {
+
+
+    protected int numberOfBeds;
+
+
+    protected int squareFeet;
+
+
+    protected double pricePerNight;
+
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq ft");
+        System.out.println("Price per night: $" + pricePerNight);
+    }
+}
+
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+
+
 public class HotelBookingApp {
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
 
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+
+        int singleRoomAvailable = 10;
+        int doubleRoomAvailable = 5;
+        int suiteRoomAvailable = 2;
+
+
+        System.out.println("---- Single Room ----");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleRoomAvailable);
+
+        System.out.println("\n---- Double Room ----");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomAvailable);
+
+        System.out.println("\n---- Suite Room ----");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomAvailable);
     }
 }
